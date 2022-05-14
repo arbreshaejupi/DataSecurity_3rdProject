04aefd19a3d006a8d7f12ad2ed5a7bfe082f0551
ArbreshaEjupi <arbresha.ejupi@student.uni-pr.edu>
2022-05-14 21:07:44 +0200

    ClientSide

--- client.py
+++ client.py
@@ -8,22 +8,20 @@ import hashlib
 
 def sendMessage(message,socket):
 	socket.send(message)  
-	
-	def connectToServer(IP, Port):                   
+
+
+def connectToServer(IP, Port):                   
 	try:
 		#print("a")
 		clientSocket = soct.socket()
 		clientSocket.connect((IP,Port)) 
-
+		
 	except ConnectionRefusedError:
 		print("Could not connect to the server")
 		clientSocket = None
 	return clientSocket
 
 message=""
-
-
-
 serverIP = '127.0.0.1'
 serverPort = 8000
 socket = connectToServer(serverIP, serverPort)
